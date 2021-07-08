479e880a5bbe4d3da8fe733e74d677cdd8ff85d5
Pilar <pbarkerit@gmail.com>
2021-07-08 18:36:35 -0400

    Did function two

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const sentence = prompt("Enter a sentence:")
 console.log(sentence)
 
+// const firstAndLast = function (sentence) {
+//     const sentence.charAt(0) + sentence.charAt(sentence.length - 1);
+// };
+
 function firstAndLast(sentence) {
-    return sentence.charAt(0) + sentence.charAt(sentence.length - 1);
+    const first = sentence.charAt(0);
+    const last = sentence.charAt(sentence.length - 1);
+    return first + last;
 }
 
-// function firstAndLast(sentence) {
-//     const first = sentence.charAt(0);
-//     const last = sentence.charAt(sentence.length - 1);
-//     return first + last;
+// const firstAndLast1 = firstAndLast.toString();
+
+// const flip = function (firstAndLast) {
+//     const flipped = firstAndLast.split("").reverse().join("");
+//     return flipped;
 // }
 
+function flip() {
+    return firstAndLast(sentence).split("").reverse().join("");
+}
+
+function 
+
+
 console.log(firstAndLast(sentence));
+console.log(flip());
