2b855ed545ae9a4596f848f9e149e5e6afe8d571
Pilar <pbarkerit@gmail.com>
2021-07-08 17:29:50 -0400

    First function

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const sentence = prompt("Enter a sentence.")
+const sentence = prompt("Enter a sentence:")
 console.log(sentence)
 
 function firstAndLast(sentence) {
-    return
+    return sentence.charAt(0) + sentence.charAt(sentence.length - 1);
 }
+
+// function firstAndLast(sentence) {
+//     const first = sentence.charAt(0);
+//     const last = sentence.charAt(sentence.length - 1);
+//     return first + last;
+// }
+
+console.log(firstAndLast(sentence));
