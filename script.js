9385e90b09ef45cfc52943e8c7519219bdf036eb
Pilar <pbarkerit@gmail.com>
2021-07-08 23:56:24 -0400

    Working on user logic

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const sentence = prompt("Enter a sentence:")
+// function getInput() {
+//     return console.log(document.getElementById("input").value);
+// }
 
+function getInput() {
+    document.getElementById("form").submit();
+}
+// const sentence = getInput()
+// console.log(sentence)
+// console.log(document.getElementById("input").value);
 
+// const sentence = document.getElementById("input").value;
+// !DO EVENT LISTENER THAT WILL FIRE UP WHEN SUBMITTED
 
 function firstAndLast(sentence) {
     const first = sentence.charAt(0);
@@ -20,11 +30,8 @@ function count(sentence) {
     const divided = Math.floor(sentence.split(" ").join("").length / 2);
     const nospaces = sentence.split(" ").join("")
     const letter = nospaces.charAt(divided)
-    return letter + connect();
+    return (letter + connect()).split("").reverse().join("");
 }
 
-console.log(sentence)
-console.log(firstAndLast(sentence));
-console.log(flip());
-console.log(connect());
-console.log(count(sentence));
+// console.log(sentence)
+// console.log(count(sentence));
