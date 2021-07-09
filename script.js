1ab2e0490356d9ef97ebbfe7c7b8bfdc6cd91ab3
Pilar <pbarkerit@gmail.com>
2021-07-08 20:35:47 -0400

    Doing fourth function

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const sentence = prompt("Enter a sentence:")
-console.log(sentence)
 
-// const firstAndLast = function (sentence) {
-//     const sentence.charAt(0) + sentence.charAt(sentence.length - 1);
-// };
+
 
 function firstAndLast(sentence) {
     const first = sentence.charAt(0);
@@ -11,19 +8,21 @@ function firstAndLast(sentence) {
     return first + last;
 }
 
-// const firstAndLast1 = firstAndLast.toString();
-
-// const flip = function (firstAndLast) {
-//     const flipped = firstAndLast.split("").reverse().join("");
-//     return flipped;
-// }
-
 function flip() {
     return firstAndLast(sentence).split("").reverse().join("");
 }
 
-function 
+function connect() {
+    return sentence + flip();
+}
 
+function count(sentence) {
+    const divided = sentence.split(" ").join("").length / 2;
+    return Math.floor(divided);
+}
 
+console.log(sentence)
 console.log(firstAndLast(sentence));
 console.log(flip());
+console.log(connect());
+console.log(count(sentence));
