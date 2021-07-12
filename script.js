379ffbd4570a0f62f86c02b4a42fae10f851add6
Pilar <pbarkerit@gmail.com>
2021-07-12 15:08:31 -0400

    Clean up js file

--- script.js
+++ script.js
@@ -1,37 +1,15 @@
-// function getInput() {
-//     return console.log(document.getElementById("input").value);
-// }
-
 console.log("hello")
 const submit = document.getElementById("submit");
-// var clearText = document.getElementById("clearText").value;
-
-
 
 submit.addEventListener("click", () => {
-    // e.preventDefault();
     var clearText = document.getElementById("clearText").value;
     console.log(clearText)
-    // console.log(firstAndLast(clearText));
-    // console.log(flip(clearText));
-    // console.log(connect(clearText));
-
     firstAndLast(clearText);
     flip(clearText);
     connect(clearText);
-    var cipher(count(clearText));
-    // console.log(count(clearText));
-    // console.log(count(sentence));
-
-})
-
-
-// const sentence = getInput()
-// console.log(sentence)z
-// console.log(document.getElementById("input").value);
-
-// const sentence = document.getElementById("input").value;
-// !DO EVENT LISTENER THAT WILL FIRE UP WHEN SUBMITTED
+    var cipher = (count(clearText));
+    console.log(count(clearText));
+});
 
 function firstAndLast(sentence) {
     const first = sentence.charAt(0);
@@ -49,12 +27,10 @@ function connect(sentence) {
 
 function count(sentence) {
     const divided = Math.floor(sentence.split(" ").join("").length / 2);
-    const nospaces = sentence.split(" ").join("")
-    const letter = nospaces.charAt(divided)
-    // console.log((letter + connect(sentence)).split("").reverse().join(""));
+    const nospaces = sentence.split(" ").join("");
+    const letter = nospaces.charAt(divided);
     return letter + connect(sentence).split("").reverse().join("");
+};
 
-}
 
-// console.log(sentence)
-// console.log(count(sentence));
+
