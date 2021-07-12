7e00266af568110b89e2c3ae1a9407072884fdc0
Pilar <pbarkerit@gmail.com>
2021-07-12 14:41:54 -0400

    Cipher algorithm connected to the front end sucessfully

--- script.js
+++ script.js
@@ -2,11 +2,32 @@
 //     return console.log(document.getElementById("input").value);
 // }
 
-function getInput() {
-    document.getElementById("form").submit();
-}
+console.log("hello")
+const submit = document.getElementById("submit");
+// var clearText = document.getElementById("clearText").value;
+
+
+
+submit.addEventListener("click", () => {
+    // e.preventDefault();
+    var clearText = document.getElementById("clearText").value;
+    console.log(clearText)
+    // console.log(firstAndLast(clearText));
+    // console.log(flip(clearText));
+    // console.log(connect(clearText));
+
+    firstAndLast(clearText);
+    flip(clearText);
+    connect(clearText);
+    var cipher(count(clearText));
+    // console.log(count(clearText));
+    // console.log(count(sentence));
+
+})
+
+
 // const sentence = getInput()
-// console.log(sentence)
+// console.log(sentence)z
 // console.log(document.getElementById("input").value);
 
 // const sentence = document.getElementById("input").value;
@@ -18,19 +39,21 @@ function firstAndLast(sentence) {
     return first + last;
 }
 
-function flip() {
+function flip(sentence) {
     return firstAndLast(sentence).split("").reverse().join("");
 }
 
-function connect() {
-    return sentence + flip();
+function connect(sentence) {
+    return sentence + flip(sentence);
 }
 
 function count(sentence) {
     const divided = Math.floor(sentence.split(" ").join("").length / 2);
     const nospaces = sentence.split(" ").join("")
     const letter = nospaces.charAt(divided)
-    return (letter + connect()).split("").reverse().join("");
+    // console.log((letter + connect(sentence)).split("").reverse().join(""));
+    return letter + connect(sentence).split("").reverse().join("");
+
 }
 
 // console.log(sentence)
